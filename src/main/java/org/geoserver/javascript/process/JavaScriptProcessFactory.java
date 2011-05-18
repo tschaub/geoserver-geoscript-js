8dcf6ca7452dade3acb3c9b113fec36bb3f25588
Tim Schaub <tim.schaub@gmail.com>
2011-05-17 22:44:34 -0600

    Package shuffle.

--- src/main/java/org/geoserver/javascript/process/JavaScriptProcessFactory.java
+++ src/main/java/org/geoserver/javascript/process/JavaScriptProcessFactory.java
@@ -2,7 +2,7 @@
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
-package org.geoserver.wps.scriptlet;
+package org.geoserver.javascript.process;
 
 import java.io.File;
 import java.io.FilenameFilter;
