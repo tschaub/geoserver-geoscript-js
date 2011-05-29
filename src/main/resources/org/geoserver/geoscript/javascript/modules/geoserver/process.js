85e4c86dc6d67603603bc85c56b3971e15bfbac1
Tim Schaub <tim.schaub@gmail.com>
2011-05-29 15:39:54 -0600

    Convert geometries in inputs and outputs.

--- src/main/resources/org/geoserver/geoscript/javascript/modules/geoserver/process.js
+++ src/main/resources/org/geoserver/geoscript/javascript/modules/geoserver/process.js
@@ -1,5 +1,17 @@
-
+var GEOM = require("geoscript/geom");
 
 exports.execute = function(process, inputs) {
-	return process.run(inputs);
+    for (var key in inputs) {
+        if (process.inputs[key].type in GEOM) {
+            inputs[key] = GEOM.Geometry.from_(inputs[key]);
+        }
+    }
+    var outputs = process.run(inputs);
+    // convert outputs
+    for (var key in outputs) {
+        if (process.outputs[key].type in GEOM) {
+            outputs[key] = outputs[key]._geometry;
+        }
+    }
+    return outputs;
 };
